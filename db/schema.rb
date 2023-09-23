feae183504a8588ebc0d35d51f2acaa96f20eb2f
Fernando Correia <fernandoacorreia@gmail.com>
2023-09-22 18:03:20 -0700

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_010139) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_010247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_010139) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tenant_id"
   end
 
   create_table "authors", force: :cascade do |t|
